58dc8c28dd5b02915b407fc68b712236ed649005
Blen <62964622+blenassefa2@users.noreply.github.com>
2023-05-15 15:28:03 +0300

    feat(backend-blen): confirm account works

---
+++ backend/src/middleware/authorization.ts
@@ -0,0 +1,47 @@
+import { Request, Response, NextFunction } from "express-serve-static-core";
+import dotenv from "dotenv";
+import jwt from "jsonwebtoken";
+import { Account } from "../resources/account/account.model";
+import { Token } from "../resources/token/token.model";
+dotenv.config();
+
+export const authorize = async (
+  req: Request,
+  res: Response,
+  next: NextFunction
+) => {
+  const authHeader = req.headers["authorization"];
+  const token = authHeader && authHeader.split(" ")[1];
+
+  if (token == null) {
+    return res.status(401).json({
+      statusCode: 401,
+      message: "Access Denied",
+    });
+  }
+
+  try {
+    const payload = jwt.verify(token, process.env.JWT_SECRET || "");
+    const { _id, role } = JSON.parse(JSON.stringify(payload));
+    const user = await Account.findById(_id);
+    if (!user) {
+      res.locals.json = {
+        data: "User is not registered",
+        statusCode: 400,
+      };
+      return next();
+    }
+
+    res.locals = {
+      _id: _id,
+      role: role,
+    };
+
+    return next();
+  } catch (error) {
+    res.status(401).json({
+      statusCode: 401,
+      message: "Invalid Token!",
+    });
+  }
+};
--- backend/src/resources/account/account.router.ts
+++ backend/src/resources/account/account.router.ts
@@ -1,10 +1,16 @@
 import { Router } from "express";
 import { respond } from "../../middleware/respond";
-import { getUserById, getUserDetails, register } from "./account.controller";
+import {
+  getAccountById,
+  updateAccountById,
+  register,
+  deleteAccountById,
+} from "./account.controller";
+import { authorize } from "../../middleware/authorization";
 const accountRouter = Router();
 
 accountRouter.post("/register", register, respond);
-accountRouter.get("/getAccountById/:id", getUserById, respond);
-// accountRouter.put("/register", register, respond);
-// accountRouter.delete("/register", register, respond);
+accountRouter.get("/getById/:id", getAccountById, respond);
+accountRouter.put("/updateById/:id", authorize, updateAccountById, respond);
+accountRouter.delete("/deleteById/:id", deleteAccountById, respond);
 export default accountRouter;
